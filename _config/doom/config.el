4f1251f0846526696f2550a15ab3c7abdeb5e144
Jose Cegri <josecegri@gmail.com>
2019-05-24 00:01:41 +0200

    minor font and emacs theme tweaks

--- _config/doom/config.el
+++ _config/doom/config.el
@@ -3,13 +3,15 @@
 ;; Place your private configuration here
 (require 'doom-themes)
 
+(set-frame-font "Iosevka 16" t t)
+
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
       doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
-(load-theme 'doom-molokai t)
+(load-theme 'doom-one t)
 
 ;; Enable flashing mode-line on errors
 (doom-themes-visual-bell-config)
