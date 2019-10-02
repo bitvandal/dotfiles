6f3e55dcac8bae2e1d19f20d64e3a0a2bb6110a3
Jose Cegri <jose.cegri@docomodigital.com>
2019-10-02 11:04:50 +0200

    trayer

--- _config/doom/modules/private/personal/config.el
+++ _config/doom/modules/private/personal/config.el
@@ -1,7 +1,7 @@
 
 ;;; ~/.config/doom/modules/private/personal/config.el -*- lexical-binding: t; -*-
 
-;; company-mode auto-completion, to turn off use :company-complete
+;; Company-mode auto-completion, to turn off use :company-complete
 
 (after! company
   (setq company-idle-delay 0.2
